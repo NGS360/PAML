04c5fe1d074077f98154ecf87c6d298f5020d228
Ryan Golhar <ryan.golhar@bms.com>
2024-02-14 13:24:40 -0500

    Add test skeleton code

---
+++ tests/test_arvados.py
@@ -0,0 +1,9 @@
+'''
+Test Module for Arvados Platform
+'''
+import unittest
+
+class TestArvadosPlaform(unittest.TestCase):
+    '''
+    Test Class for Arvados Platform
+    '''
---
+++ tests/test_sevenbridges.py
@@ -0,0 +1,9 @@
+'''
+Test Module for SevenBridges Platform
+'''
+import unittest
+
+class TestSevenBridgesPlaform(unittest.TestCase):
+    '''
+    Test Class for SevenBridges Platform
+    '''
