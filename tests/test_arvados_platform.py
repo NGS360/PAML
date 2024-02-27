8c647009b0eed914ace7c3ea0fa4be52b2ab4695
Ryan Golhar <ryan.golhar@bms.com>
2024-02-27 09:37:19 -0500

    Rewrite tests for cleaner mocking

--- tests/test_arvados_platform.py
+++ tests/test_arvados_platform.py
@@ -13,6 +13,8 @@ class TestArvadosPlaform(unittest.TestCase):
     '''
     def setUp(self) -> None:
         self.platform = ArvadosPlatform('Arvados')
+        self.platform.api = MagicMock()
+        self.platform.keep_client = MagicMock()
         return super().setUp()
 
     @mock.patch("arvados.api_from_config")
@@ -28,17 +30,14 @@ class TestArvadosPlaform(unittest.TestCase):
         self.platform.connect()
         self.assertTrue(self.platform.connected)
 
-    @mock.patch("arvados.api_from_config")
-    def test_delete_task(self, mock_arvados_api):
+    def test_delete_task(self):
         ''' Test delete_task method '''
         # Set up mocks
-        mock_arvados_api.return_value = MagicMock()
         task = ArvadosTask(container_request={"uuid": "12345"}, container={"uuid": "67890"})
         # Test
-        self.platform.connect()
         self.platform.delete_task(task)
         # Assert
-        mock_arvados_api.return_value.container_requests().delete.assert_called_once_with(uuid="12345")
+        self.platform.api.container_requests().delete.assert_called_once_with(uuid="12345")
 
 if __name__ == '__main__':
     unittest.main()
