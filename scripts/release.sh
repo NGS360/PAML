26518c84b364f62805a58a4963c27b1044965dba
Ryan Golhar <ryan.golhar@bms.com>
2024-02-28 13:05:11 -0500

    Formalize deployment process, update README.md

---
+++ scripts/release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+TAG=`grep version pyproject.toml  | cut -d '=' -f2 | sed 's/"//g'`
+
+read -p "Creating new release for $TAG. Do you want to continue? [Y/n] " prompt
+
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
+    python scripts/prepare_changelog.py $TAG
+    git add -A
+    git commit -m "Bump version to $TAG for release" || true && git push
+    echo "Creating new git tag $TAG"
+    git tag "$TAG" -m "$TAG"
+    git push --tags
+else
+    echo "Cancelled"
+    exit 1
+fi
+
