000957d2ccc21df0975b09520b0f6908f0cda6d3
Ryan Golhar <ryan.golhar@bms.com>
2024-02-28 13:19:17 -0500

    Make sure tag/release has v

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,10 +9,9 @@ if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]
     git add -A
     git commit -m "Bump version to $TAG for release" || true && git push
     echo "Creating new git tag $TAG"
-    git tag "$TAG" -m "$TAG"
+    git tag "v$TAG" -m "v$TAG"
     git push --tags
 else
     echo "Cancelled"
     exit 1
 fi
-
