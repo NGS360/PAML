a2f2398fd3c28bc7697f484d8ec6286542a0d128
chienm2 <ming-shan.chien@bms.com>
2024-03-21 17:38:27 +0000

    update unitest submit_task for sbg

--- tests/test_sevenbridges_platform.py
+++ tests/test_sevenbridges_platform.py
@@ -35,5 +35,64 @@ class TestSevenBridgesPlaform(unittest.TestCase):
         # Assert
         task.delete.assert_called_once_with()
 
+    def test_submit_task(self):
+        ''' Test submit_task method '''
+        # Set up test parameters
+        # Set up mocks
+        name = "test_task"
+        project = "test_project"
+        workflow = "test_workflow"
+        parameters = {
+            'capture_regions': {
+                'class': 'File',
+                'path': '65fc33432348e03d3a73d727'
+            },
+            'sequence_dictionary': {
+                'class': 'File',
+                'path': '65fba49b337d9b7faeff9d15'
+            },
+            'gatk_panel_of_normals': {
+                'class': 'File',
+                'path': '65fc34004eef1760f3f443c4'
+            },
+            'chromosome_sizes': {
+                'class': 'File',
+                'path': '65fb8f27337d9b7faeff9d0f'
+            },
+            'snp_file': {
+                'class': 'File',
+                'path': '65fb8f27337d9b7faeff9cfd'
+            },
+            'in_reference': {
+                'class': 'File',
+                'path': '65fb8f27337d9b7faeff9d00'
+            },
+            'sex': 'XY',
+            'mate_orientation_control': 'FR',
+            'mate_orientation_sample': 'FR',
+            'noisy_data': True,
+            'ploidy': [2],
+            'memory_per_job_alleliccounts': 30000,
+            'memory_per_job_modelsegments': 32000,
+            'bin_length': 1000,
+            'tumor_bam': {
+                'class': 'File',
+                'path': '65eb61ac7aaf1d5a95e3d581',
+                'secondaryFiles': [
+                    {'class': 'File', 'path': '65eb61ac7aaf1d5a95e3d581.bai'},
+                    {'class': 'File', 'path': '65eb61ac7aaf1d5a95e3d581'}
+                ]
+            }
+        } 
+        # Test
+        task = self.platform.submit_task(
+            name,
+            project,
+            workflow,
+            parameters
+        )
+        # Assert
+        task.run.assert_called_once_with()
+
 if __name__ == '__main__':
     unittest.main()
