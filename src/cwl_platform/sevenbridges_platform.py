5b75502d3dd9c6f1c73ae5c41c12992d661a0ba6
Aleksandar Baburski <aleksandar.baburski@sbgenomics.com>
2024-05-15 12:05:51 +0200

    move platform settings from platform_config.py to sevenbridges_platform.py

--- src/cwl_platform/sevenbridges_platform.py
+++ src/cwl_platform/sevenbridges_platform.py
@@ -7,7 +7,36 @@ import sevenbridges
 from sevenbridges.http.error_handlers import rate_limit_sleeper, maintenance_sleeper, general_error_sleeper
 
 from .base_platform import Platform
-from .platform_config import sb_credentials, sb_execution_settings
+
+
+class Config:
+    credentials = {
+        'US': {
+            'api_endpoint': 'https://bms-api.sbgenomics.com/v2',
+            'token': 'dummy',
+            'profile': 'default'
+        },
+        'CN': {
+            'api_endpoint': 'https://api.sevenbridges.cn/v2',
+            'token': 'dummy',
+            'profile': 'china'
+        }
+    }
+
+    execution_settings = {
+        'US': {
+            'use_elastic_disk': True,
+            'use_memoization': True,
+            'use_spot_instance': True,
+            "max_parallel_instances": 1
+        },
+        'CN': {
+            'use_elastic_disk': False,
+            'use_memoization': True,
+            'use_spot_instance': True,
+            "max_parallel_instances": 1
+        }
+    }
 
 
 class SevenBridgesPlatform(Platform):
@@ -561,15 +590,15 @@ class SevenBridgesPlatform_US(SevenBridgesPlatform):
         Initialize SevenBridges Platform US
         '''
         super().__init__(name)
-        self.api_endpoint = sb_credentials['US']['api_endpoint']
-        self.token = sb_credentials['US']['token']
-        self.use_elastic = sb_execution_settings['US']['use_elastic_disk']
-        self.use_memoization = sb_execution_settings['US']['use_memoization']
+        self.api_endpoint = Config.credentials['US']['api_endpoint']
+        self.token = Config.credentials['US']['token']
+        self.use_elastic = Config.execution_settings['US']['use_elastic_disk']
+        self.use_memoization = Config.execution_settings['US']['use_memoization']
 
         if not self._session_id:
             if os.path.exists(os.path.expanduser("~") + '/.sevenbridges/credentials') is True:
                 self.api_config = sevenbridges.Config(
-                    profile=sb_credentials['US']['profile']
+                    profile=Config.credentials['US']['profile']
                 )
             else:
                 raise ValueError('No SevenBridges credentials found')
@@ -586,8 +615,8 @@ class SevenBridgesPlatform_US(SevenBridgesPlatform):
         session_id = os.environ.get('SESSION_ID')
         if session_id:
             try:
-                api = sevenbridges.Api(url=sb_credentials['US']['api_endpoint'],
-                                       token=sb_credentials['US']['token'],
+                api = sevenbridges.Api(url=Config.credentials['US']['api_endpoint'],
+                                       token=Config.credentials['US']['token'],
                                        advance_access=True)
                 api._session_id = session_id
                 myself = api.users.me()
@@ -603,15 +632,15 @@ class SevenBridgesPlatform_CN(SevenBridgesPlatform):
         Initialize SevenBridges Platform CN
         '''
         super().__init__(name)
-        self.api_endpoint = sb_credentials['CN']['api_endpoint']
-        self.token = sb_credentials['CN']['token']
-        self.use_elastic = sb_execution_settings['CN']['use_elastic_disk']
-        self.use_memoization = sb_execution_settings['CN']['use_memoization']
+        self.api_endpoint = Config.credentials['CN']['api_endpoint']
+        self.token = Config.credentials['CN']['token']
+        self.use_elastic = Config.execution_settings['CN']['use_elastic_disk']
+        self.use_memoization = Config.execution_settings['CN']['use_memoization']
 
         if not self._session_id:
             if os.path.exists(os.path.expanduser("~") + '/.sevenbridges/credentials') is True:
                 self.api_config = sevenbridges.Config(
-                    profile=sb_credentials['CN']['profile']
+                    profile=Config.credentials['CN']['profile']
                 )
             else:
                 raise ValueError('No SevenBridges credentials found')
@@ -628,8 +657,8 @@ class SevenBridgesPlatform_CN(SevenBridgesPlatform):
         session_id = os.environ.get('SESSION_ID')
         if session_id:
             try:
-                api = sevenbridges.Api(url=sb_credentials['CN']['api_endpoint'],
-                                       token=sb_credentials['CN']['token'],
+                api = sevenbridges.Api(url=Config.credentials['CN']['api_endpoint'],
+                                       token=Config.credentials['CN']['token'],
                                        advance_access=True)
                 api._session_id = session_id
                 myself = api.users.me()
