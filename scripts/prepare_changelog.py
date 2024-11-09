bf66af3583d55428191e6828330cded9161a110c
Ryan Golhar <ryan.golhar@bms.com>
2024-11-09 11:52:15 -0500

    Fix pylint issues in scripts/prepare_changelog.py

--- scripts/prepare_changelog.py
+++ scripts/prepare_changelog.py
@@ -1,19 +1,23 @@
+'''
+This script creates a CHANGELOG.md based on the git history from the last version tag.
+'''
 from datetime import datetime
 from pathlib import Path
 import sys
 
 #from my_package.version import VERSION
-VERSION=sys.argv[1]
+REPO=sys.argv[1]
+VERSION=sys.argv[2]
 
 def main():
+    ''' main function '''
     changelog = Path("CHANGELOG.md")
 
-    with changelog.open() as f:
+    with changelog.open(encoding='ascii') as f:
         lines = f.readlines()
 
     insert_index: int = -1
-    for i in range(len(lines)):
-        line = lines[i]
+    for i, line in enumerate(lines):
         if line.startswith("## Unreleased"):
             insert_index = i + 1
         elif line.startswith(f"## [v{VERSION}]"):
@@ -28,11 +32,11 @@ def main():
     lines.insert(insert_index, "\n")
     lines.insert(
         insert_index + 1,
-        f"## [v{VERSION}](https://github.com/NGS360/PAML/releases/tag/v{VERSION}) - "
+        f"## [v{VERSION}]({REPO}/releases/tag/v{VERSION}) - "
         f"{datetime.now().strftime('%Y-%m-%d')}\n",
     )
 
-    with changelog.open("w") as f:
+    with changelog.open("w", encoding="ascii") as f:
         f.writelines(lines)
 
 
