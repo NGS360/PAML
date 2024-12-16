30baa2e92daa898d984981cb9daf153848ec9c7a
Ryan Golhar <ryan.golhar@bms.com>
2024-12-16 18:33:13 -0500

    Continue to support python 3.9

--- run_tests_in_docker.sh
+++ run_tests_in_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # Run using:
-# docker run --rm -ti -v $PWD:/cwl_platform -w /cwl_platform python:3.13 ./run_tests_in_docker.sh
+# docker run --rm -ti -v $PWD:/cwl_platform -w /cwl_platform python:3.9 ./run_tests_in_docker.sh
 
 python3 -m venv ~/env
 source ~/env/bin/activate
