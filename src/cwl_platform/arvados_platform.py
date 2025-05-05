ec5df8fe8ba15bd215784554ffe0189a59693023
vasques1 <Steven.VasquezGrinnell@bms.com>
2025-05-05 19:05:42 -0400

    tmp logging - generator -> list

--- src/cwl_platform/arvados_platform.py
+++ src/cwl_platform/arvados_platform.py
@@ -749,10 +749,10 @@ class ArvadosPlatform(Platform):
             )
 
         tasks = []
-        container_requests = arvados.util.keyset_list_all(
+        container_requests = list(arvados.util.keyset_list_all(
             self.api.container_requests().list,
             filters=filters
-        )
+        ))
         self.logger.info("Found %d container requests: %s",
                          len(container_requests), container_requests)
         for container_request in container_requests:
