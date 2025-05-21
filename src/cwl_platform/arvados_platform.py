13d9e4ee565ca8d31f6eb2745ac842e3b30f5305
vasques1 <Steven.VasquezGrinnell@bms.com>
2025-05-21 13:22:28 -0400

    rm temp overly verbose logging

--- src/cwl_platform/arvados_platform.py
+++ src/cwl_platform/arvados_platform.py
@@ -749,13 +749,10 @@ class ArvadosPlatform(Platform):
             )
 
         tasks = []
-        container_requests = list(arvados.util.keyset_list_all(
+        for container_request in arvados.util.keyset_list_all(
             self.api.container_requests().list,
             filters=filters
-        ))
-        self.logger.info("Found %d container requests: %s",
-                         len(container_requests), container_requests)
-        for container_request in container_requests:
+        ):
             # Get the container
             container = self.api.containers().get(
                 uuid=container_request['container_uuid']).execute()
